9874fe682b25ad756f5287d6651ab00f196746a4
Sofia Soto <A01620155@itesm.mx>
2022-09-20 09:52:26 -0500

    Add DynamicDropdown.java for WebElementsAutomation package

--- SeleniumCourse/SeleniumCourse/src/test/java/WebElementsAutomation/CompoundDropDown.java
+++ SeleniumCourse/SeleniumCourse/src/test/java/WebElementsAutomation/CompoundDropDown.java
@@ -12,26 +12,26 @@ public class CompoundDropDown {
 		// TODO Auto-generated method stub
 		
 		//set webdriver object
-				System.setProperty("webdriver.chrome.driver", "C:\\Users\\Usuario\\Desktop\\Automation\\WebDrivers\\chromedriver.exe");
-				WebDriver driver = new ChromeDriver();
-				
-				//implicit wait
-				driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-				driver.manage().window().maximize();
-				
-				//open website
-				driver.get("https://www.spicejet.com/");
-				
-				//click the dropdown
-				driver.findElement(By.xpath("//div[@data-testid='home-page-travellers']")).click();
-				
-				//select number of people (adults)
-				for(int i =0; i<8; i++) {
-					driver.findElement(By.xpath("//div[@data-testid='Adult-testID-plus-one-cta']")).click();
-				}
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Usuario\\Desktop\\Automation\\WebDrivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
 
-				//Exit from browser
-				driver.quit();
+		//implicit wait
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+
+		//open website
+		driver.get("https://www.spicejet.com/");
+
+		//click the dropdown
+		driver.findElement(By.xpath("//div[@data-testid='home-page-travellers']")).click();
+
+		//select number of people (adults)
+		for(int i =0; i<8; i++) {
+			driver.findElement(By.xpath("//div[@data-testid='Adult-testID-plus-one-cta']")).click();
+		}
+
+		//Exit from browser
+		driver.quit();
 
 	}
 
