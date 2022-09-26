c460fcb008424d1897161b891247ad81a470f80a
Sofia Soto <A01620155@itesm.mx>
2022-09-26 12:30:21 -0500

    Configure TestNg project to start that section

---
+++ SeleniumCourse/TestNgTutorial/src/test/java/Lessons/TestNgClass1.java
@@ -0,0 +1,13 @@
+package Lessons;
+
+import org.testng.annotations.Test;
+
+public class TestNgClass1 {
+	
+	
+	@Test
+	public void test1() {
+		System.out.println("Hello I am the first test!");
+	}
+
+}
