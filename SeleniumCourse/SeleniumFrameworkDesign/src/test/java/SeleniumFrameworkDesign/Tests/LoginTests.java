f5a95070f5aa8d7e9b9061861b5df557d54cda3d
claud <claud@DESKTOP-GDB468S>
2022-10-03 10:27:52 -0500

    Parametrize login test cases using data provider

--- SeleniumCourse/SeleniumFrameworkDesign/src/test/java/SeleniumFrameworkDesign/Tests/LoginTests.java
+++ SeleniumCourse/SeleniumFrameworkDesign/src/test/java/SeleniumFrameworkDesign/Tests/LoginTests.java
@@ -1,25 +1,86 @@
 package SeleniumFrameworkDesign.Tests;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import SeleniumFrameworkDesign.TestComponents.BaseTest;
 
 public class LoginTests extends BaseTest{
-	
-	
-	@Test
-	public void verifyCorrectLogin() {
-		loginObj.loginApplication("claudio.soto.ayala@gmail.com", "Legostarwars10.");
+
+
+	@Test(dataProvider = "getCorrectData")
+	public void verifyCorrectLogin(String user, String password) {
+		loginObj.loginApplication(user, password);
 		Assert.assertTrue(loginObj.getLoginLogo());
-		
+
 	}
-	
-	
-	@Test
-	public void verifyIncorrectLoginMessage() {
-		loginObj.loginApplication("not.an.email@gmail.com", "incorrect.");
+
+	@Test(dataProvider = "getIncorrectData")
+	public void verifyIncorrectLoginMessage(String user, String password) {
+		loginObj.loginApplication(user, password);
 		Assert.assertEquals(loginObj.getLoginErrorMessage(), "Incorrect email or password.");
+	}
+
+
+	//using regular format
+	/*
+	@DataProvider
+	public Object[][] getCorrectData(){
+
+		Object[][] data = new Object[3][2];
+
+		//correct password and user
+		data[0][0] = "claudio.soto.ayala@gmail.com";
+		data[0][1] = "Legostarwars10.";
+
+		//correct password and user
+		data[1][0] = "claudio.soto.ayala@gmail.com";
+		data[1][1] = "Legostarwars10.";
+
+		//correct password and user
+		data[2][0] = "claudio.soto.ayala@gmail.com";
+		data[2][1] = "Legostarwars10.";
+
+		return data;
+	}
+	 */
+
+	//using json  format
+	@DataProvider
+	public Object[][] getCorrectData(){
+		return new Object[][] {{"claudio.soto.ayala@gmail.com","Legostarwars10."},{"claudio.soto.ayala@gmail.com","Legostarwars10."}
+		,{"claudio.soto.ayala@gmail.com","Legostarwars10."}};
+	}
+
+	//using regular format
+	/*
+	@DataProvider
+	public Object[][] getIncorrectData(){
+
+		Object[][] data = new Object[3][2];
+
+		//correct password and user
+		data[0][0] = "not.a.valid@gmail.com";
+		data[0][1] = "Legostarwars10.";
+
+		//correct password and user
+		data[1][0] = "claudio.soto.ayala@gmail.com";
+		data[1][1] = "NotavalidPassword.";
+
+		//correct password and user
+		data[2][0] = "not.a.valid@gmail.com";
+		data[2][1] = "NotavalidPassword.";
+
+		return data;
+	}
+	 */
+
+	//using json format
+	@DataProvider
+	public Object[][] getIncorrectData(){
+		return new Object[][] {{"not.a.valid@gmail.com","Legostarwars10."},{"claudio.soto.ayala@gmail.com","NotavalidPassword."}
+		,{"not.a.valid@gmail.com","NotavalidPassword."}};
 	}
 
 }
