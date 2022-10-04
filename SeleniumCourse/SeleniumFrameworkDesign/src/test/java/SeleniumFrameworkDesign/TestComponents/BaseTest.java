496d24c368a69e268c5a2af1f535a64fd48c64fd
claud <claud@DESKTOP-GDB468S>
2022-10-04 14:19:06 -0500

    Create profiles to execute from terminal different xml execution files

--- SeleniumCourse/SeleniumFrameworkDesign/src/test/java/SeleniumFrameworkDesign/TestComponents/BaseTest.java
+++ SeleniumCourse/SeleniumFrameworkDesign/src/test/java/SeleniumFrameworkDesign/TestComponents/BaseTest.java
@@ -14,12 +14,9 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
-
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -28,75 +25,101 @@ import SeleniumFrameworkDesign.PageObjects.LoginPageObject;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
-	
+
 	//declare global webdriver
 	public WebDriver driver;
 	public LoginPageObject loginObj;
 
-	
+
 	public WebDriver initializeDriver() throws IOException {
 		//delcare fileInputStream for global properties object, if you need to share the project is necessary to fix the path to get local path
 		//FileInputStream globalProperties = new FileInputStream("C:\\Users\\Usuario\\Desktop\\Automation\\SeleniumCourse\\SeleniumFrameworkDesign\\src\\test\\java\\SeleniumFrameworkDesign\\Resources\\GlobalProperties.properties");
 		FileInputStream globalProperties = new FileInputStream(System.getProperty("user.dir") + "\\src\\test\\java\\SeleniumFrameworkDesign\\Resources\\GlobalProperties.properties");
-		
+
 		//declare properties object and load file properties
 		Properties prop = new Properties();
 		prop.load(globalProperties);
 		String browser = prop.getProperty("browser");
-		
-		
+
+		//to execute in terminal
+		//String browser_maven_terminal = System.getProperty("browser");
+
+		//selects maven value if not null
+		//String browserIfTerminal = terminalBrowserCheck(browser_maven_terminal, browser);
+
+		//if(driver == null) {
+		/*
+		if(browserIfTerminal.equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}else if (browserIfTerminal.equalsIgnoreCase("firefox")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}else if (browserIfTerminal.equalsIgnoreCase("edge")){
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+		*/
+		//}
+
 		if(browser.equalsIgnoreCase("chrome")) {
 			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
 		}else if (browser.equalsIgnoreCase("firefox")) {
-			//for firefox
-		}else {
-			//for edge
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}else if (browser.equalsIgnoreCase("edge")){
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
 		}
-		
+
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://rahulshettyacademy.com/client/");
 
 		return driver;
 	}
-	
+
 	@BeforeMethod
 	public LoginPageObject launchApp() throws IOException {
 		driver = initializeDriver();
 		loginObj = new LoginPageObject(driver);
 		return loginObj;
 	}
-	
+
 	@AfterMethod
 	public void closeApp() {
 		driver.quit();
 	}
 
 	/*UTILITIES */
-	
+
+	public String terminalBrowserCheck(String terminalBrowser, String propertiesBrowser) {
+		return terminalBrowser != null ? terminalBrowser : propertiesBrowser;
+	}
+
 	public List<HashMap<String, String>> getJsonDataToHashMap(String filePath) throws IOException {
 		//get jason file into a string
 		String jsonContet = FileUtils.readFileToString(new File (filePath),
 				StandardCharsets.UTF_8);
-	
+
 		//get hashmap with the json
 		ObjectMapper mapper = new ObjectMapper();
-		
+
 		//json to hashmap list
 		List<HashMap<String,String>> data = mapper.readValue(jsonContet, new TypeReference<List<HashMap<String,String>>>(){});
-		
+
 		return data;
 	}
-	
-	
+
+
 	public String getScreenShot(String testCaseName, WebDriver driver) throws IOException {
 		TakesScreenshot ts = (TakesScreenshot) driver;
 		File source= ts.getScreenshotAs(OutputType.FILE);
 		File file = new File(System.getProperty("user.dir") + "\\test-output\\" + testCaseName + ".png");
 		FileUtils.copyFile(source, file);
 		return System.getProperty("user.dir") + "\\test-output\\" + testCaseName + ".png";
-		
+
 	}
 
 }
